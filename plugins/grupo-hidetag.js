467eb7cb91d0cfb60fd07475ea05e3aa04a897b0
Juan Gustavo A <gustavoaguilas149@gmail.com>
2024-11-13 15:34:58 -0600

    Update hidetag

--- plugins/grupo-hidetag.js
+++ plugins/grupo-hidetag.js
@@ -14,7 +14,7 @@ let masss = more.repeat(850)
 let htextos = `${text ? text : " *🐈‍⬛ Holis :3* "}`
 
 await conn.sendMessage(m.chat, { text : text ? text : `*✨${await conn.getName(m.chat)}✨*` , mentions: users}, { quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
-    let stiker = await sticker(imagen12, false, global.packname, global.author)
+    let stiker = await sticker(imagen13, false, global.packname, global.author)
     await conn.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, {
     })
 return
