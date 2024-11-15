aed5cead311af4e30d865cab3322d6ab59d3c374
Juan Gustavo A <gustavoaguilas149@gmail.com>
2024-11-15 12:55:16 -0600

    tests

--- plugins/testcomandos.js
+++ plugins/testcomandos.js
@@ -1,49 +1,23 @@
-import FormData from "form-data";
-import Jimp from "jimp";
-const handler = async (m, {conn, usedPrefix, command}) => {
-    try {
-        let q = m.quoted ? m.quoted : m;
-        let mime = (q.msg || q).mimetype || q.mediaType || "";
-        if (!mime) throw `╰⊱❗️⊱ *𝙇𝙊 𝙐𝙎𝙊́ 𝙈𝘼𝙇 | 𝙐𝙎𝙀𝘿 𝙄𝙏 𝙒𝙍𝙊𝙉𝙂* ⊱❗️⊱╮\n\n𝙀𝙉𝙑𝙄𝙀 𝙐𝙉𝘼 𝙄𝙈𝘼𝙂𝙀𝙉 𝙊 𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝘼 𝘼 𝙐𝙉𝘼 𝙄𝙈𝘼𝙂𝙀𝙉 𝘾𝙊𝙉 𝙀𝙇 𝘾𝙊𝙈𝘼𝙉𝘿𝙊 ${usedPrefix + command}`;
-        if (!/image\/(jpe?g|png)/.test(mime)) throw `╰⊱⚠️⊱ *𝘼𝘿𝙑𝙀𝙍𝙏𝙀𝙉𝘾𝙄𝘼 | 𝙒𝘼𝙍𝙉𝙄𝙉𝙂* ⊱⚠️⊱╮\n\nEL FORMATO DEL ARCHIVO (${mime}) NO ES COMPATIBLE, ENVÍA O RESPONDE A UNA FOTO`;
-        m.reply("*✨ 𝙈𝙀𝙅𝙊𝙍𝘼𝙉𝘿𝙊 𝙇𝘼 𝘾𝘼𝙇𝙄𝘿𝘼𝘿...*");
-        let stiker = await sticker(imagen13, false, global.packname, global.author)
-        m.reply(m.chat, stiker, 'sticker.webp', null, m, false, {
-        })
-        let img = await q.download?.();
-        let pr = await remini(img, "enhance");
-        conn.sendMessage(m.chat, {image: pr}, {quoted: m});
-    } catch {
-        throw "╰⊱⚠️⊱ *𝘼𝘿𝙑𝙀𝙍𝙏𝙀𝙉𝘾𝙄𝘼 | 𝙒𝘼𝙍𝙉𝙄𝙉𝙂* ⊱⚠️⊱╮\n\n𝙁𝘼𝙇𝙇𝙊, 𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍 𝙑𝙐𝙀𝙇𝙑𝘼 𝘼 𝙄𝙉𝙏𝙀𝙉𝙏𝘼𝙍";
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
+import { sticker } from '../lib/sticker.js'
+import * as fs from 'fs';
+
+const handler = async (m, { conn, text, participants, isOwner, isAdmin, args }) => {
+    const allowedNumber = '5215614236722'; // Número permitido
+
+    // Verificamos si el número que envió el mensaje es el permitido
+    if (m.sender !== allowedNumber) {
+        return m.react("❌");
     }
-};
 
-handler.command = ["reminin", "hdn", "enhancen"];
-export default handler;
-
-async function remini(imageData, operation) {
-    return new Promise(async (resolve, reject) => {
-        const availableOperations = ["enhance", "recolor", "dehaze"];
-        if (availableOperations.includes(operation)) {
-            operation = operation;
-        } else {
-            operation = availableOperations[0];
-        }
-        const baseUrl = "https://inferenceengine.vyro.ai/" + operation + ".vyro";
-        const formData = new FormData();
-        formData.append("image", Buffer.from(imageData), {filename: "enhance_image_body.jpg", contentType: "image/jpeg"});
-        formData.append("model_version", 1, {"Content-Transfer-Encoding": "binary", contentType: "multipart/form-data; charset=utf-8"});
-        formData.submit({url: baseUrl, host: "inferenceengine.vyro.ai", path: "/" + operation, protocol: "https:", headers: {"User-Agent": "okhttp/4.9.3", Connection: "Keep-Alive", "Accept-Encoding": "gzip"}},
-            function (err, res) {
-                if (err) reject(err);
-                const chunks = [];
-                res.on("data", function (chunk) {chunks.push(chunk)});
-                res.on("end", function () {resolve(Buffer.concat(chunks))});
-                res.on("error", function (err) {
-                    reject(err);
-                });
-            },
-        );
-    });
+    // Generamos el sticker y lo enviamos
+    let stiker = await sticker(imagen13, false, global.packname, global.author);
+    await conn.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { mentions: users }, { quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100 });
 }
+
+handler.command = /^(yo|t)$/i
+handler.group = true
+handler.admin = true
+export default handler
+
 
