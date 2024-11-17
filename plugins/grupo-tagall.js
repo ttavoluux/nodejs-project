2e04f55c6c773c8a1242f6dfc442b4e96f91f6e9
Juan Gustavo A <gustavoaguilas149@gmail.com>
2024-11-16 22:34:53 -0600

    update tagall

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -24,7 +24,7 @@ teks += ` *${texto}* ${texto3}${text.toUpperCase()}${texto4} \n\n`
 for (let mem of participants) {
 teks += `*@${mem.id.split('@')[0]}* `}
 teks += `\n`
-teks += `\n *${packname} ${vs}* \n${numParticipantes}`
+teks += `\n *${packname} ${vs}*`
 //teks += '\n *𝓑𝔂: 𝓐𝓻𝓵𝓮𝓽𝓼𝓲𝓽𝓪 𝓫𝓸𝓽 💕* '
 //teks += '\n\n*💜 Nuevos comandos ⚠️*\n     *.aviso*\n     *.todosprem*'
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
