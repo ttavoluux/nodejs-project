4e6a7f69a7c57dfa8b97375266004cdc79332486
Juan Gustavo A <gustavoaguilas149@gmail.com>
2025-02-01 15:56:06 -0600

    add change group

---
+++ plugins/switchgrup.js
@@ -0,0 +1,39 @@
+let handler = async (m, { conn, command, usedPrefix, text }) => {
+    let message = ``;
+
+    // Si no se pasa un link, enviamos las instrucciones
+    if (!text) {
+        message = `📍 *Instrucciones para cambiar de grupo*
+
+> 1.enviar link .cambiargrupo [link]
+
+> 2. *El bot enviará una solicitud de cambio*
+
+> 3. *Después de enviar la solicitud, el bot ingresara al nuevo grupo*
+
+🌸Recuerda usar correctamente *ArletteBot Commutity Edition* ✨`;
+    } else {
+        // Obtener el número del bot (el JID)
+        const botNumber = conn.user.jid;  // El número del bot es el JID
+
+        // const gruponame = conn.getName(m.chat);
+        // Enviar el mensaje con la solicitud al mismo número del bot
+        await conn.sendMessage(botNumber, `El grupo: ${await conn.getName(m.chat)} solicito un cambio a: ${text} `);
+
+        // Enviar confirmación al grupo
+        let solienviada = '📌 *INFO: Solicitud de cambio recibida correctamente* ✅ \n' +
+            '\n' +
+            '> ⏱️ En max 48hrs se realizara el cambio ❗'
+        await conn.sendMessage(m.chat, imagen12, solienviada);
+
+        // Salir del grupo
+        // await conn.groupLeave(m.chat);
+    }
+
+    // Enviar la imagen si es necesario
+    // await conn.sendFile(m.chat, imagen12, 'lp.jpg', message);
+}
+
+handler.command = /^(changegroup|cambiargrupo)$/i;
+export default handler;
+
